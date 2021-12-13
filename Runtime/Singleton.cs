9f90b61225da343f466d3c8533a31c8b39849f5d
Aarquie <zero@aarquie.com>
2021-12-13 01:00:37 -0500

    Added namespace

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,35 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+namespace AarquieSoltuions.Base.Singleton
 {
-    private static T instance;
-    public static T Instance 
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = GameObject.FindObjectOfType<T>();
-            }
+        private static T instance;
 
-            if (instance == null)
+        public static T Instance
+        {
+            get
             {
+                if (instance == null)
+                {
+                    instance = GameObject.FindObjectOfType<T>();
+                }
+
+                if (instance != null)
+                {
+                    return instance;
+                }
+                
                 GameObject container = new GameObject($"[{typeof(T)}]");
                 instance = container.AddComponent<T>();
-            }
 
-            return instance;
+                return instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
         }
     }
-
-
-    public virtual void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
-    
 }
